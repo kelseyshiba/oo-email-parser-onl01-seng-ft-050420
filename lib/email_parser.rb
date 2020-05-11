ae53472dcf1c662e48849ba16f07f71ea09e17bb
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 15:42:23 +0000

    Done.

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,12 +11,25 @@ class EmailAddressParser
     @emails = emails
   end
   
-    
   def parse
-    parsed = @emails.split(/,| /).filter {|a| a != ""}
-    @@emails << parsed
-    @@emails.uniq.flatten
+    # parsed = @emails.split(/,| /).filter {|a| a != ""}
+    emails.split.map {|email| email.split(/,| /)}.flatten.uniq
   end
-  
+    
+# def parse
+#     parsed = @emails.split(/,| /).filter {|a| a != ""}
+#     @@emails << parsed
+#     @@emails.flatten.uniq
+#   end
 
+  # def parse
+  #   parsed = @emails.split(/,| /).filter {|a| a !=""}
+  #   parsed.collect do |dups|
+  #     if dups
+  #       @@emails << dups
+  #     end
+  #   end
+  #   @@emails
+  # end
 end
+
