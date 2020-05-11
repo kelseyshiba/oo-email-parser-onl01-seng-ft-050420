26fff3d1a3a865cce2a91ad1d0225d31888bca88
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-11 15:04:16 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -21,7 +21,7 @@ class EmailAddressParser
 
   def parse
     parsed = @emails.split(/,| /).filter {|a| a !=""}
-    new_emails = parsed.uniq
+    new_emails = parsed.uniq.flatten
     @@emails << new_emails
   end
 end
